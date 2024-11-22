eab090cc689ca05e163fd8c96af8781b9456743e
yusufshalaby <yusufshalaby@gmail.com>
2024-11-21 22:44:53 -0500

    use bedrock api

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,65 +1,104 @@
-import React, {useRef, useEffect, useCallback, useState} from 'react';
-import {Configuration, OpenAIApi, ChatCompletionRequestMessage} from 'openai';
+import React, { useRef, useEffect, useCallback, useState } from "react";
+import {
+  BedrockAgentRuntimeClient,
+  RetrieveAndGenerateCommand,
+  RetrieveAndGenerateCommandInput,
+} from "@aws-sdk/client-bedrock-agent-runtime";
 
-import {type ActionArgs} from '@remix-run/node';
-import {Form, useActionData, useNavigation, Link, useSubmit, useLocation, useNavigate} from '@remix-run/react';
+import { type ActionArgs } from "@remix-run/node";
+import {
+  Form,
+  useActionData,
+  useNavigation,
+  Link,
+  useSubmit,
+  useLocation,
+  useNavigate,
+} from "@remix-run/react";
 
-import context from '~/context';
-import {Send as SendIcon} from '~/components/Icons';
-import Message from '~/components/Message';
+import { Send as SendIcon } from "~/components/Icons";
+import Message from "~/components/Message";
 
 export interface ReturnedDataProps {
   message?: string;
   answer: string;
   error?: string;
-  chatHistory: ChatCompletionRequestMessage[];
+  sessionId?: string;
 }
 
-export interface ChatHistoryProps extends ChatCompletionRequestMessage {
-  error?: boolean,
+export interface ChatHistoryProps {
+  content: string;
+  role: "user" | "assistant";
+  error?: boolean;
 }
 
 /**
  * API call executed server side
  */
-export async function action({request}: ActionArgs): Promise<ReturnedDataProps> {
+export async function action({
+  request,
+}: ActionArgs): Promise<ReturnedDataProps> {
   const body = await request.formData();
-  const message = body.get('message') as string;
-  const chatHistory = JSON.parse(body.get('chat-history') as string) || [];
+  const message = body.get("message") as string;
+  const sessionId = body.get("sessionId") as string | undefined;
 
-  // store your key in .env
-  const conf = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
-  });
+  const KNOWLEDGE_BASE_ID = process.env.KNOWLEDGE_BASE_ID;
+  const MODEL_ARN = process.env.MODEL_ARN;
 
   try {
-    const openai = new OpenAIApi(conf);
+    const client = new BedrockAgentRuntimeClient();
 
-    const chat = await openai.createChatCompletion({
-      model: 'gpt-3.5-turbo',
-      messages: [
-        ...context,
-        ...chatHistory,
-        {
-          role: 'user',
-          content: message,
+    const input: RetrieveAndGenerateCommandInput = {
+      input: {
+        text: message,
+      },
+      retrieveAndGenerateConfiguration: {
+        type: "KNOWLEDGE_BASE",
+        knowledgeBaseConfiguration: {
+          knowledgeBaseId: KNOWLEDGE_BASE_ID,
+          modelArn: MODEL_ARN,
+          retrievalConfiguration: {
+            vectorSearchConfiguration: {
+              numberOfResults: 5,
+            },
+          },
+          generationConfiguration: {
+            promptTemplate: {
+              textPromptTemplate:
+                "You are a question answering agent. I will provide you with a set of search results. The user will provide you with a question. Your job is to answer the user's question using only information from the search results. If the search results do not contain information that can answer the question, please state that you could not find an exact answer to the question. \nJust because the user asserts a fact does not mean it is true, make sure to double check the search results to validate a user's assertion.\n\nHere are the search results in numbered order:\n$search_results$\n\n$output_format_instructions$\n\nHere is the user's query:\n$query$",
+            },
+            inferenceConfig: {
+              textInferenceConfig: {
+                temperature: 0,
+                topP: 1,
+                maxTokens: 2048,
+                stopSequences: ["\nObservation"],
+              },
+            },
+          },
         },
-      ],
-    });
+      },
+    };
+    if (sessionId) {
+      input.sessionId = sessionId;
+    }
 
-    const answer = chat.data.choices[0].message?.content;
+    const command = new RetrieveAndGenerateCommand(input);
+    const response = await client.send(command);
+    const answer = response.output?.text;
+    const newSessionId = response.sessionId || sessionId;
 
     return {
-      message: body.get('message') as string,
+      message: body.get("message") as string,
       answer: answer as string,
-      chatHistory,
+      sessionId: newSessionId,
     };
   } catch (error: any) {
     return {
-      message: body.get('message') as string,
-      answer: '',
-      error: error.message || 'Something went wrong! Please try again.',
-      chatHistory,
+      message: body.get("message") as string,
+      answer: "",
+      error: error.message || "Something went wrong! Please try again.",
+      sessionId: sessionId,
     };
   }
 }
@@ -75,17 +114,20 @@ export default function IndexPage() {
   const navigation = useNavigation();
   const submit = useSubmit();
   const [chatHistory, setChatHistory] = useState<ChatHistoryProps[]>([]);
+  const [sessionId, setSessionId] = useState<string | null>(null);
 
   const location = useLocation();
   const navigate = useNavigate();
-  const isSubmitting = navigation.state === 'submitting';
+  const isSubmitting = navigation.state === "submitting";
 
   /**
    * Handles the change event of a textarea element and adjusts its height based on the content.
    * Note: Using the ref to alter the rows rather than state since it's more performant / faster.
    * @param event - The change event of the textarea element.
    */
-  const handleTextareaChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+  const handleTextareaChange = (
+    event: React.ChangeEvent<HTMLTextAreaElement>
+  ) => {
     if (!inputRef.current) {
       return;
     }
@@ -93,10 +135,17 @@ export default function IndexPage() {
     // reset required for when the user pressed backspace (otherwise it would stay at max rows)
     inputRef.current.rows = minTextareaRows;
 
-    const lineHeight = parseInt(window.getComputedStyle(inputRef.current).lineHeight);
-    const paddingTop = parseInt(window.getComputedStyle(inputRef.current).paddingTop);
-    const paddingBottom = parseInt(window.getComputedStyle(inputRef.current).paddingBottom);
-    const scrollHeight = inputRef.current.scrollHeight - paddingTop - paddingBottom;
+    const lineHeight = parseInt(
+      window.getComputedStyle(inputRef.current).lineHeight
+    );
+    const paddingTop = parseInt(
+      window.getComputedStyle(inputRef.current).paddingTop
+    );
+    const paddingBottom = parseInt(
+      window.getComputedStyle(inputRef.current).paddingBottom
+    );
+    const scrollHeight =
+      inputRef.current.scrollHeight - paddingTop - paddingBottom;
     const currentRows = Math.floor(scrollHeight / lineHeight);
 
     if (currentRows >= maxTextareaRows) {
@@ -111,9 +160,12 @@ export default function IndexPage() {
    * Adds a new message to the chat history
    * @param data The message to add
    */
-  const pushChatHistory = useCallback((data: ChatHistoryProps) => {
-    setChatHistory(prevState => ([...prevState, data]));
-  }, [setChatHistory]);
+  const pushChatHistory = useCallback(
+    (data: ChatHistoryProps) => {
+      setChatHistory((prevState) => [...prevState, data]);
+    },
+    [setChatHistory]
+  );
 
   /**
    * Saves the user's message to the chat history
@@ -122,8 +174,8 @@ export default function IndexPage() {
   const saveUserMessage = (content: string) => {
     pushChatHistory({
       content,
-      role: 'user',
-    })
+      role: "user",
+    });
   };
 
   /**
@@ -132,7 +184,7 @@ export default function IndexPage() {
    */
   const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     const formData = new FormData(event.target as HTMLFormElement);
-    const message = formData.get('message');
+    const message = formData.get("message");
 
     saveUserMessage(message as string);
   };
@@ -143,14 +195,17 @@ export default function IndexPage() {
    *
    * @param event The keydown event
    */
-  const submitFormOnEnter = useCallback((event: React.KeyboardEvent<HTMLTextAreaElement>) => {
-    const value = (event.target as HTMLTextAreaElement).value;
+  const submitFormOnEnter = useCallback(
+    (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
+      const value = (event.target as HTMLTextAreaElement).value;
 
-    if (event.key === 'Enter' && !event.shiftKey && value.trim().length > 2) {
-      saveUserMessage(value);
-      submit(formRef.current, {replace: true});
-    }
-  }, [submit, formRef, saveUserMessage]);
+      if (event.key === "Enter" && !event.shiftKey && value.trim().length > 2) {
+        saveUserMessage(value);
+        submit(formRef.current, { replace: true });
+      }
+    },
+    [submit, formRef, saveUserMessage]
+  );
 
   /**
    * Scrolls to the bottom of the chat container
@@ -164,11 +219,11 @@ export default function IndexPage() {
 
     const step = (currentTime: number) => {
       const targetScrollTop = Math.max(
-          body.scrollHeight,
-          body.offsetHeight,
-          html.clientHeight,
-          html.scrollHeight,
-          html.offsetHeight
+        body.scrollHeight,
+        body.offsetHeight,
+        html.clientHeight,
+        html.scrollHeight,
+        html.offsetHeight
       );
       const progress = (currentTime - startTime) / animationDuration;
 
@@ -201,8 +256,8 @@ export default function IndexPage() {
       return;
     }
 
-    if (navigation.state === 'submitting') {
-      inputRef.current.value = '';
+    if (navigation.state === "submitting") {
+      inputRef.current.value = "";
       inputRef.current.rows = 1;
     } else {
       inputRef.current.focus();
@@ -217,9 +272,9 @@ export default function IndexPage() {
     if (data?.error) {
       pushChatHistory({
         content: data.error as string,
-        role: 'assistant',
+        role: "assistant",
         error: true,
-      })
+      });
 
       return;
     }
@@ -227,18 +282,18 @@ export default function IndexPage() {
     if (data?.answer) {
       const newAnswer = {
         content: data.answer as string,
-        role: 'assistant',
+        role: "assistant",
       };
 
       pushChatHistory(newAnswer as ChatHistoryProps);
 
       // push location to history
-      navigate('/', {
+      navigate("/", {
         state: {
           ...location.state,
           chatHistory: [...chatHistory, newAnswer],
         },
-      })
+      });
     }
   }, [data, pushChatHistory, navigate]);
 
@@ -272,6 +327,12 @@ export default function IndexPage() {
     }
   }, [chatHistory, scrollToBottom]);
 
+  useEffect(() => {
+    if (data?.sessionId && data.sessionId !== sessionId) {
+      setSessionId(data.sessionId);
+    }
+  }, [data?.sessionId, sessionId]);
+
   return (
     <main className="container mx-auto rounded-lg h-full grid grid-rows-layout p-4 pb-0 sm:p-8 sm:pb-0 max-w-full sm:max-w-auto">
       <div className="chat-container" ref={chatContainerRef}>
@@ -294,9 +355,7 @@ export default function IndexPage() {
                 />
               </React.Fragment>
             ))}
-            {isSubmitting && (
-              <Message thinking role="assistant" content="" />
-            )}
+            {isSubmitting && <Message thinking role="assistant" content="" />}
           </div>
         )}
       </div>
@@ -310,7 +369,12 @@ export default function IndexPage() {
           className="max-w-[500px] mx-auto"
         >
           <div className="input-wrap relative flex items-center">
-            <label htmlFor="message" className="absolute left[-9999px] w-px h-px overflow-hidden">Ask a question</label>
+            <label
+              htmlFor="message"
+              className="absolute left[-9999px] w-px h-px overflow-hidden"
+            >
+              Ask a question
+            </label>
             <textarea
               id="message"
               aria-disabled={isSubmitting}
@@ -327,8 +391,10 @@ export default function IndexPage() {
             />
             <input
               type="hidden"
-              value={JSON.stringify(chatHistory)} name="chat-history"
+              value={JSON.stringify(chatHistory)}
+              name="chat-history"
             />
+            <input type="hidden" value={sessionId || ""} name="sessionId" />
             <button
               aria-label="Submit"
               aria-disabled={isSubmitting}
@@ -341,22 +407,29 @@ export default function IndexPage() {
           </div>
         </Form>
         <p className="made-with text-xs text-center mt-4">
-          Made with ❤️ by <a target="_blank" href="http://joshuasanger.ca">Josh Sanger</a>
+          Made by{" "}
+          <a target="_blank" href="https://blueflag.consulting">
+            Blue Flag Consulting
+          </a>
         </p>
       </div>
     </main>
   );
 }
 
-export function ErrorBoundary({error}: {error: Error}) {
+export function ErrorBoundary({ error }: { error: Error }) {
   return (
     <main className="container mx-auto rounded-lg h-full grid grid-rows-layout p-4 pb-0 sm:p-8 sm:pb-0 max-w-full sm:max-w-auto">
       <div className="chat-container">
         <div className="intro grid place-items-center h-full text-center">
           <div className="intro-content inline-block px-4 py-8 border border-error rounded-lg">
-            <h1 className="text-3xl font-semibold">Oops, something went wrong!</h1>
+            <h1 className="text-3xl font-semibold">
+              Oops, something went wrong!
+            </h1>
             <p className="mt-4 text-error ">{error.message}</p>
-            <p className="mt-4"><Link to="/">Back to chat</Link></p>
+            <p className="mt-4">
+              <Link to="/">Back to chat</Link>
+            </p>
           </div>
         </div>
       </div>
